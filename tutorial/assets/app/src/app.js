89b699bf4d924d05535ecb33f8bb265d18dfc837
Moritz Lübken <moritz.luebken@codemeta.de>
2023-05-07 20:59:44 +0200

    Update app to display dog facts

--- tutorial/assets/app/src/app.js
+++ tutorial/assets/app/src/app.js
@@ -6,8 +6,10 @@ const bodyParser = require("koa-bodyparser");
 const views = require("koa-views");
 const path = require("path");
 const sqlite = require("better-sqlite3");
-// const DataFrame = require('pandas-js');
-// const Matrix = require('ml-matrix');
+const { parse } = require("csv-parse");
+const { readFileSync, createReadStream } = require("fs");
+const loadCSV = require("./load_csv");
+const { readFile } = require("fs/promises");
 
 const app = new Koa();
 const router = new Router();
@@ -19,15 +21,6 @@ app.use(session(app));
 // Initialize the body parser middleware
 app.use(bodyParser())
 
-// Initialize the views middleware with the EJS engine
-app.use(
-  views(`${__dirname}/views`, {
-    map: {
-      html: "pug",
-    },
-  })
-);
-
 // Initialize the SQLite database
 const db = sqlite(path.join(__dirname, "database.sqlite"));
 
@@ -38,9 +31,20 @@ const stmt = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)");
 //Set admin password to 123456
 stmt.run("admin", "password123");
 
+router.get("/", async (ctx, next) => {
+  const { user } = ctx.session;
+  if (!user) {
+    ctx.redirect("/login");
+    return;
+  }
+  ctx.redirect("/dashboard");
+});
+
 // Define the login route
 router.get("/login", async (ctx, next) => {
-  await ctx.render("login");
+  const login = await readFile(path.join(__dirname, "views/login.html"), "utf8");
+  ctx.set("Content-Type", "text/html");
+  ctx.body = login;
 });
 
 // Define the authentication route
@@ -65,47 +69,29 @@ router.get("/dashboard", async (ctx, next) => {
     ctx.redirect("/login");
     return;
   }
-  await ctx.render("dashboard", { user });
+  const dashboard = await readFile(path.join(__dirname, "views/dashboard.html"), "utf8");
+  ctx.set("Content-Type", "text/html");
+  ctx.body = dashboard;
 });
 
 // Serve static files
 app.use(serve(path.join(__dirname, "public")));
 
-// Use the router middleware
-app.use(router.routes());
 
-// Load data
-//const df = new DataFrame.fromCSV('scotch.csv', { separator: ';' });
-
-// Create cosine similarity model
-//const distances = new Matrix(df.values).cosineSimilarity();
-//const simdf = new DataFrame(distances, df.index, df.index);
-
-//function getSimScotch(name) {
-  // Pick out the top 5 most similar scotches (exclude the first one, which is the scotch itself)
-//  const simScores = simdf.get(name).sort('descending').iloc(1, 6);
-//  const recommendations = {};
-//  for (const [i, score] of simScores.entries()) {
-//    recommendations[df.get(i, 'NAME')] = score.toFixed(2);
- // }
-//  return recommendations;
-// }
-
-// app.get('/', (req, res) => {
-//   res.render('dashboard', { whiskeys: df.get('NAME').values, recommendations: {} });
-// });
-
-// app.get('/sims/:brand', (req, res) => {
-//   const brand = req.params.brand;
-//   if (df.get('NAME').values.includes(brand)) {
-//     const results = getSimScotch(brand);
-//     res.render('dashboard', { whiskeys: df.get('NAME').values, recommendations: results });
-//   } else {
-//     res.render('dashboard', { whiskeys: df.get('NAME').values, recommendations: { "Something": "went wrong in the sims method" } });
-//   }
-// });
-
-// Start the server
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+console.log("Loading CSV...");
+loadCSV().then((res) => {
+  router.get("/data", async (ctx, next) => {
+    const { user } = ctx.session;
+    if (!user) {
+      ctx.unauthorized();
+      return;
+    }
+    ctx.body = res;
+  });
+  // Use the router middleware
+  app.use(router.routes());
+  // Start the server
+  app.listen(3000, () => {
+    console.log("Server started on port 3000");
+  });
 });
